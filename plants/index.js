178e1c68caab1786f96d5db24e3948ac85d6d8a7
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-01-30 23:15:40 +0300

    feat: add blur effect for cards groups

--- plants/index.js
+++ plants/index.js
@@ -1,38 +1,88 @@
-(function () {
-  console.log(
-    "Общая оценка - 85 баллов. \n1. Вёрстка соответствует макету. Ширина экрана 768px +24 \n2. Вёрстка соответствует макету. Ширина экрана 380px +24 \n3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. +15 \n4. На ширине экрана 380рх и меньше реализовано адаптивное меню +22"
-  );
-})();
+// (function () {
+//   console.log(
+//     "Общая оценка - 85 баллов. \n1. Вёрстка соответствует макету. Ширина экрана 768px +24 \n2. Вёрстка соответствует макету. Ширина экрана 380px +24 \n3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. +15 \n4. На ширине экрана 380рх и меньше реализовано адаптивное меню +22"
+//   );
+// })();
 
-(function () {
-  const body = document.querySelector(".body");
-  const burgerIcon = document.querySelector(".burger");
-  const menu = document.querySelector(".header-block__nav");
-  const menuCloseIcon = document.querySelector(".header-block__nav-close");
-  const menuItems = document.querySelector(".nav-list");
-  const shadow = document.querySelector(".header-block__burger-shadow");
+/* BURGER MENU START */
 
-  burgerIcon.addEventListener("click", () => {
-    menu.classList.add("header-block__nav_active");
-    shadow.classList.add("shadow-active");
-    body.classList.add("no-scroll");
-  });
+const body = document.querySelector(".body");
+const burgerIcon = document.querySelector(".burger");
+const menu = document.querySelector(".header-block__nav");
+const menuCloseIcon = document.querySelector(".header-block__nav-close");
+const menuItems = document.querySelector(".nav-list");
+const shadow = document.querySelector(".header-block__burger-shadow");
 
-  menuCloseIcon.addEventListener("click", () => {
-    menu.classList.remove("header-block__nav_active");
-    shadow.classList.remove("shadow-active");
-    body.classList.remove("no-scroll");
-  });
+burgerIcon.addEventListener("click", () => {
+  menu.classList.add("header-block__nav_active");
+  shadow.classList.add("shadow-active");
+  body.classList.add("no-scroll");
+});
 
-  shadow.addEventListener("click", () => {
-    menu.classList.remove("header-block__nav_active");
-    shadow.classList.remove("shadow-active");
-    body.classList.remove("no-scroll");
-  });
+menuCloseIcon.addEventListener("click", () => {
+  menu.classList.remove("header-block__nav_active");
+  shadow.classList.remove("shadow-active");
+  body.classList.remove("no-scroll");
+});
 
-  menuItems.addEventListener("click", () => {
-    menu.classList.remove("header-block__nav_active");
-    shadow.classList.remove("shadow-active");
-    body.classList.remove("no-scroll");
-  });
-})();
+shadow.addEventListener("click", () => {
+  menu.classList.remove("header-block__nav_active");
+  shadow.classList.remove("shadow-active");
+  body.classList.remove("no-scroll");
+});
+
+menuItems.addEventListener("click", () => {
+  menu.classList.remove("header-block__nav_active");
+  shadow.classList.remove("shadow-active");
+  body.classList.remove("no-scroll");
+});
+
+/* BURGER MENU END */
+
+/* SECTION SERVICE INTERACTIVE BEHAVIOR START */
+
+const gardenBtn = document.querySelector(".garden-btn");
+const lawnBtn = document.querySelector(".lawn-btn");
+const plantingBtn = document.querySelector(".planting-btn");
+const gardenCard = document.querySelectorAll(".garden");
+const lawnCard = document.querySelectorAll(".lawn");
+const plantingCard = document.querySelectorAll(".planting");
+
+function gardens() {
+  if (gardenBtn.getAttribute("value") === "off") {
+    gardenBtn.setAttribute("value", "on");
+  } else {
+    gardenBtn.setAttribute("value", "off");
+  }
+  gardenBtn.classList.toggle("active-btn");
+  gardenCard.forEach((card) => card.classList.toggle("blur-effect"));
+};
+
+gardenBtn.addEventListener("click", gardens, false); // вешает слушатель
+
+function lawns() {
+  if (lawnBtn.getAttribute("value") === "off") {
+    lawnBtn.setAttribute("value", "on");
+  } else {
+    lawnBtn.setAttribute("value", "off");
+  }
+  lawnBtn.classList.toggle("active-btn");
+  lawnCard.forEach((card) => card.classList.toggle("blur-effect"));
+};
+
+lawnBtn.addEventListener("click", lawns, false);
+
+function plantings() {
+  if (plantingBtn.getAttribute("value") === "off") {
+    plantingBtn.setAttribute("value", "on");
+  } else {
+    plantingBtn.setAttribute("value", "off");
+  }
+  plantingBtn.classList.toggle("active-btn");
+  plantingCard.forEach((card) => card.classList.toggle("blur-effect"));
+};
+
+plantingBtn.addEventListener("click", plantings, false);
+
+
+/* SECTION SERVICE INTERACTIVE BEHAVIOR END */
